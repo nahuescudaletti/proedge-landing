0ae042c96c90f559f9f1612f8989d5fef3819433
nahuescudaletti <nahueby17@gmail.com>
2024-06-27 18:40:54 -0300

    netifly

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  output: 'standalone',
-  distDir: 'dist',
-  assetPrefix: isProd ? '/proedge-landing/' : '',
-  images: {
-    unoptimized: true,
-  },
-  // ... otras configuraciones que ya tengas
+    output: 'standalone',
+    distDir: 'dist',
+    assetPrefix: isProd ? '' : '',
+    images: {
+        unoptimized: true,
+    },
+    // ... otras configuraciones que ya tengas
 };
 
 module.exports = nextConfig;
