e9b4cf7a0d74ec8ff5028e31505284c6db627f0e
nahuescudaletti <nahueby17@gmail.com>
2024-06-27 19:16:00 -0300

    again...

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    output: 'standalone',
-    distDir: 'dist', // Asegúrate de que esta configuración sea compatible con tu estructura de proyecto
-    assetPrefix: isProd ? '' : '',
-    images: {
-        loader: 'custom', // Asegúrate de que estás usando un loader personalizado si es necesario
-        path: isProd ? '/proedge-landing/_next/image' : '/_next/image', // Ajusta según el entorno de producción
-        domains: ['example.com'], // Agrega tus dominios de imágenes si es necesario
-        // otras configuraciones de imágenes
-    },
-    // otras configuraciones específicas de Next.js
+  output: 'standalone',
+  distDir: 'dist',
+  assetPrefix: isProd ? '/proedge-landing/' : '',
+  images: {
+    unoptimized: true,
+  },
+  // ... otras configuraciones que ya tengas
 };
 
 module.exports = nextConfig;
