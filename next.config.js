54b751240b39887a9aa2ccb1cbcf52d7f600eb77
nahuescudaletti <nahueby17@gmail.com>
2024-06-27 19:03:25 -0300

    netifly.toml

--- next.config.js
+++ next.config.js
@@ -2,12 +2,15 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     output: 'standalone',
-    distDir: 'dist',
+    distDir: 'dist', // Asegúrate de que esta configuración sea compatible con tu estructura de proyecto
     assetPrefix: isProd ? '' : '',
     images: {
-        unoptimized: true,
+        loader: 'custom', // Asegúrate de que estás usando un loader personalizado si es necesario
+        path: isProd ? '/proedge-landing/_next/image' : '/_next/image', // Ajusta según el entorno de producción
+        domains: ['example.com'], // Agrega tus dominios de imágenes si es necesario
+        // otras configuraciones de imágenes
     },
-    // ... otras configuraciones que ya tengas
+    // otras configuraciones específicas de Next.js
 };
 
 module.exports = nextConfig;
