587e8d7eb64f24b77deccd31ba40c5b6aba9ce8b
nahuescudaletti <nahueby17@gmail.com>
2024-06-28 15:43:59 -0300

    fix

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c37a00070c7c8a75"; }
+/******/ 		__webpack_require__.h = function() { return "6f948b01bcdc7d9d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
